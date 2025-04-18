f8ff251ae028ddbb9f33dd2280c1521007ec7798
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2025-04-19 00:46:03 +0900

    ディレクトリ整理

--- bskylink/src/db/migrations/provider.ts
+++
@@ -1,8 +0,0 @@
-import type { Migration, MigrationProvider } from "kysely";
-
-export class DbMigrationProvider implements MigrationProvider {
-	constructor(private migrations: Record<string, Migration>) {}
-	async getMigrations(): Promise<Record<string, Migration>> {
-		return this.migrations;
-	}
-}
--- bskyogcard/src/components/Img.tsx
+++
@@ -1,6 +0,0 @@
-import type React from "react";
-
-export function Img(props: Omit<React.ImgHTMLAttributes<HTMLImageElement>, "src"> & { src: Buffer }) {
-	const { src, ...others } = props;
-	return <img {...others} src={`data:image/jpeg;base64,${src.toString("base64")}`} />;
-}
